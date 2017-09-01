571c89060243f044a6915b19f58bd1a794274ef4
maud <maud.dehaas@student.rocvf.nl>
2017-09-01 19:52:02 +0200

    een huis met een deur een raam en een dak

--- src/demo/Show.java
+++ src/demo/Show.java
@@ -24,11 +24,30 @@ public class Show extends Applet {
     }
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        setBackground(Color.blue);
-        g.setColor(Color.yellow);
-        g.drawString("Maud!!", 50, 60 );
+        //int i = 60;
+        //setBackground(Color.lightGray);
+       // g.setColor(Color.black);
+      /*  for (i=60 ; i< 300; i=i+10) {
+            g.drawString("Maud!!", 50, 60);
         g.drawString("de Haas!", 50, 71 );
+      */
+        g.setColor(Color.red);
+        int[] xPoints = {100,40,150};
+        int[] yPoints = {150,200,200};
+        g.drawPolygon(xPoints, yPoints, 3);
+        g.fillPolygon(xPoints, yPoints, 3);
 
+        g.setColor(Color.green);
+        g.fillRect(40,200, 110, 100);
+        g.drawRect(40,200, 110, 100);
+
+        g.setColor(Color.blue);
+        g.fillRect(80,230,50,30);
+        g.drawRect(80,230,50,30);
+
+        g.setColor(Color.yellow);
+        g.fillRect(50,250,20,60);
+        g.drawRect(50,250,20,60);
     }
 
 
