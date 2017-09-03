35aa481b05fbf8d305417e492c096c6e675caa00
maud <maud.dehaas@student.rocvf.nl>
2017-09-03 16:20:52 +0200

    stoplicht

---
+++ src/HO1234/Naam.java
@@ -0,0 +1,20 @@
+package HO1234;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Naam extends Applet {
+
+    public void init() {
+    }
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Maud", 50, 60 );
+
+        g.setColor(Color.red);
+        g.drawString("de Haas", 50, 70 );
+
+
+    }
+
+}
