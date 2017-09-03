340e5aa75f66ead6dd8edef91fda0dc8cbe2c02b
maud <maud.dehaas@student.rocvf.nl>
2017-09-03 17:01:49 +0200

    dobbelsteen toegevoegd

---
+++ src/HO1234/figuren.java
@@ -0,0 +1,18 @@
+package HO1234;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class figuren extends Applet {
+
+
+        public void init() {
+        }
+
+        public void paint(Graphics g) {
+
+            g.setColor(Color.black);
+            g.drawString(100,100,100,);
+
+        }
+}
