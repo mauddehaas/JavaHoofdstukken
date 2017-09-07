923d4f32a5570a892d2152f3d739649950edc3f2
maud <maud.dehaas@student.rocvf.nl>
2017-09-07 14:38:06 +0200

    staafdiagra is af

---
+++ src/HO6/Berekenen.java
@@ -0,0 +1,23 @@
+package HO6;
+
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class Berekenen extends Applet {
+    int prijsjan
+
+
+    public void init (){
+
+
+    }
+    public void paint(Graphics g) {
+
+    }
+
+}
+
+
+
