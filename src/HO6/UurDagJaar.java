d0f6b58ff95f722f9a6c891a5dd87d4a4ce61c57
maud <maud.dehaas@student.rocvf.nl>
2017-09-08 10:09:07 +0200

    commit

--- src/HO6/UurDagJaar.java
+++ src/HO6/UurDagJaar.java
@@ -1,4 +1,40 @@
 package HO6;
 
-public class UurDagJaar {
+import java.applet.Applet;
+import java.awt.*;
+
+public class UurDagJaar extends Applet{
+
+    int uur;
+    int dag;
+    int jaar;
+    int uitkomst;
+
+    public void init () {
+
+        uitkomst=60;
+        uur=uitkomst*60;
+        dag=uitkomst*60*24;
+        jaar=uitkomst*365*24*60;
+
+
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawString("in 1 uur zitten"+uur,20,20);
+        g.drawString("in 1 dag zitten"+dag,20,40);
+        g.drawString("in 1 jaar zitten"+jaar,20,60);
+        g.drawString("seconden",160,20);
+        g.drawString("seconden",160,40);
+        g.drawString("seconden",160,60);
+
+    }
 }
+
+
+
+
+
+
+
