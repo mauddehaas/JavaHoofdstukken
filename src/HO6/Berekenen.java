f3bfeede2e2d7ab02b1b3465b8dc0efa2812ef13
maud <maud.dehaas@student.rocvf.nl>
2017-09-08 09:27:36 +0200

    berekenen is af aleen geen , getal

--- src/HO6/Berekenen.java
+++ src/HO6/Berekenen.java
@@ -6,14 +6,35 @@ import java.awt.*;
 
 
 public class Berekenen extends Applet {
-    int prijsjan
+
+     double prijsjan,prijsali, prijsjeannette,prijsmaud,prijsontvangst;
 
 
     public void init (){
+        prijsontvangst=(113/4);
+        prijsali= prijsontvangst;
+        prijsjan= prijsontvangst;
+        prijsjeannette= prijsontvangst;
+        prijsmaud= prijsontvangst;
+
+
+
 
 
     }
     public void paint(Graphics g) {
+
+        g.setColor(Color.blue);
+
+        g.drawString("Jan krijgt:" + prijsjan,20,20);
+        g.drawString("Ali krijgt:" + prijsali,20,35);
+        g.drawString("Jeannette krijgt:" + prijsjeannette,20,50);
+        g.drawString("Maud krijgt:" + prijsmaud,20,65);
+
+
+
+
+
 
     }
 
