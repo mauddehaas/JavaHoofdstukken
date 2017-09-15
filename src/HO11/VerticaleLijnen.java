6a9a23ca6f9e8bd104cd6f2e7befa66364b4649d
maud <maud.dehaas@student.rocvf.nl>
2017-09-15 12:41:09 +0200

    HO11 11.1 af

---
+++ src/HO11/VerticaleLijnen.java
@@ -0,0 +1,21 @@
+package HO11;
+
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class VerticaleLijnen extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int teller;
+        int y = 0;
+
+        for(teller = 0; teller < 10; teller++) {
+            y += 20;
+            g.drawLine(50 , y, 300, y );
+        }
+    }
+}
