9567cb0c14635d597b465898beca35e80a9d6b75
maud <maud.dehaas@student.rocvf.nl>
2017-09-20 12:05:49 +0200

    HO11 11.11 af

--- src/HO11/PraktijkOpdracht.java
+++ src/HO11/PraktijkOpdracht.java
@@ -1,7 +1,42 @@
 package HO11;
 
-public class PraktijkOpdracht {
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PraktijkOpdracht extends Applet{
+    TextField tekstvak;
+    Label label;
+    String s;
+    int getal,tafel,uitkomst;
+
+    public void init(){
+        tekstvak=new TextField("",20);
+        tekstvak.addActionListener(new tekstvakListener());
+        add (tekstvak);
+        label=new Label("typ een cijfer tussen de 1 en 10");
+        add (label);
+
+    }
 
 
 
+    public void paint(Graphics g){
+        int y=20;
+
+        for (int getal = 1; getal < 11; getal++) {
+            y += 40;
+            uitkomst = getal * tafel;
+            g.drawString(getal + "X" + tafel + "=" +uitkomst , 50 , y);
+
+        }
+    }
+    class tekstvakListener implements ActionListener{
+        public void actionPerformed (ActionEvent e){
+            s=tekstvak.getText();
+            tafel=Integer.parseInt(s);
+            repaint();
+        }
+    }
 }
